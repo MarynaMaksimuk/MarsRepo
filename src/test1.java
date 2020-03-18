c5d6419a488ca3eb373e4cec2b116585c11d5869
Maryna <maksimarina@gmail.com>
2020-03-17 20:31:05 -0400

    project and class created

--- src/test1.java
+++ src/test1.java
@@ -3,7 +3,23 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int[]numbers = {34,67,3,93,56,72,17,33};
+		int largest = numbers[0];
+		int small= numbers[0];
+		int total=0;
+		for(int num:numbers) {
+			if (num>largest)
+				largest=num;
+			
+				if (num<small) {
+					small=num;
+				}
+				total+=num;
+			}
+			System.out.println("largest "+ largest);
+			System.out.println("Smallest "+small);
+			System.out.println("the sum of all numbers "+total);
+		}}
 
-	}
 
-}
